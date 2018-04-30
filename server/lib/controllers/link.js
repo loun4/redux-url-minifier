96c716741f037272f36eb5bbd2f8b69ef4e4231e
Iheb <iheb02@yahoo.fr>
2018-04-30 10:26:37 +0200

    [api] Refacto, db as argument for LinkPackage

--- server/lib/controllers/link.js
+++ server/lib/controllers/link.js
@@ -6,37 +6,33 @@ const { credentials } = require('../config');
 const { errorHandler, NotFoundError } = require('../utils/error');
 const shortener = require('../utils/shortener');
 const validate = require('../utils/validator');
-
-const {
-  Link,
-  LinkSchema,
-  LoadLinks,
-  LoadByLinkURL,
-  LoadById,
-} = require('../models/link');
-
+const LinkPackage = require('../models/link');
 
 const LinkRoutes = (db) => {
+  const {
+    Link,
+    LinkSchema,
+    LoadLinks,
+    LoadByLinkURL,
+    LoadById,
+  } = LinkPackage(db);
+
   const router = Router();
 
   router.post('/link', validate(LinkSchema), (req, res, next) => {
-    const linksCollection = db.getCollection('links');
-
-    let link = LoadByLinkURL(linksCollection, req.body.linkURL);
+    let link = LoadByLinkURL(req.body.linkURL);
     if (link !== null) {
       return res.send(link.toJSON());
     }
 
     link = new Link(req.body);
-    return link.create(linksCollection)
+    return link.create()
       .then(link => res.send(link.toJSON()))
       .catch(next);
   });
 
   router.get('/link', basicAuth(credentials), (req, res) => {
-    const linksCollection = db.getCollection('links');
-    const links = LoadLinks(linksCollection).map(link => link.toJSON());
-
+    const links = LoadLinks().map(link => link.toJSON());
     res.send(links);
   });
 
@@ -46,14 +42,12 @@ const LinkRoutes = (db) => {
       return next({ name: NotFoundError });
     }
 
-    const linksCollection = db.getCollection('links');
-
-    const link = LoadById(linksCollection, id);
+    const link = LoadById(id);
     if (!link) {
       return next({ name: NotFoundError });
     }
 
-    return link.remove(linksCollection)
+    return link.remove()
       .then(() => res.send({}))
       .catch(next);
   });
--- server/lib/models/link.js
+++ server/lib/models/link.js
@@ -5,98 +5,105 @@ const shortener = require('../utils/shortener');
 const DbInsertError = 'db insert error';
 const DbRemoveError = 'db insert error';
 
-const LinkSchema = {
-  type: 'object',
-  required: ['linkURL'],
-  properties: {
-    id: {
-      type: ['string', 'null'],
-      default: null,
+const LinkPackage = (db) => {
+  const collection = db.getCollection('links');
+
+  const LinkSchema = {
+    type: 'object',
+    required: ['linkURL'],
+    properties: {
+      id: {
+        type: ['string', 'null'],
+        default: null,
+      },
+      linkURL: {
+        type: 'string',
+        format: 'uri',
+        default: '',
+      },
     },
-    linkURL: {
-      type: 'string',
-      format: 'uri',
-      default: '',
-    },
-  },
+  };
+
+  const { properties: definitions } = LinkSchema;
+
+  class Link {
+    constructor(raw) {
+      const {
+        $loki = definitions.id.default, // raw lokiJS ID
+        meta = { created: null }, // raw lokiJS meta, include create date
+        linkURL = definitions.linkURL.default,
+      } = raw;
+
+      this.properties = {
+        id: shortener.encode($loki),
+        linkURL,
+      };
+
+      this.rawId = $loki;
+      this.meta = meta;
+    }
+
+    create() {
+      return new Promise((resolve, reject) => {
+        const doc = collection.insert(this.toDbDocument());
+        if (!doc) {
+          return reject({ name: InternalError, errors: DbInsertError });
+        }
+
+        return resolve(new Link(doc));
+      });
+    }
+
+    remove() {
+      return new Promise((resolve, reject) => {
+        const doc = collection.remove(this.toDbDocument());
+        if (!doc) {
+          return reject({ name: InternalError, errors: DbRemoveError });
+        }
+
+        return resolve({});
+      });
+    }
+
+    toDbDocument() {
+      const { linkURL } = this.properties;
+
+      return {
+        ...this.rawId && { $loki: this.rawId },
+        linkURL,
+      };
+    }
+
+    toJSON() {
+      return {
+        ...this.properties,
+        meta: { ...this.meta },
+      };
+    }
+  }
+
+  const LoadById = (id) => {
+    const doc = collection.get(id);
+    return doc === null ? null : new Link(doc);
+  };
+
+  const LoadByLinkURL = (linkURL) => {
+    const doc = collection.findOne({ linkURL });
+    return doc === null ? null : new Link(doc);
+  };
+
+  const LoadLinks = () =>
+    collection.find().map(doc => new Link(doc));
+
+
+  return {
+    LinkSchema,
+    Link,
+    LoadByLinkURL,
+    LoadLinks,
+    LoadById,
+  };
 };
 
-const { properties: definitions } = LinkSchema;
 
-class Link {
-  constructor(raw) {
-    const {
-      $loki = definitions.id.default, // raw lokiJS ID
-      meta = { created: null }, // raw lokiJS meta, include create date
-      linkURL = definitions.linkURL.default,
-    } = raw;
-
-    this.properties = {
-      id: shortener.encode($loki),
-      linkURL,
-    };
-
-    this.rawId = $loki;
-    this.meta = meta;
-  }
-
-  create(collection) {
-    return new Promise((resolve, reject) => {
-      const doc = collection.insert(this.toDbDocument());
-      if (!doc) {
-        return reject({ name: InternalError, errors: DbInsertError });
-      }
-
-      return resolve(new Link(doc));
-    });
-  }
-
-  remove(collection) {
-    return new Promise((resolve, reject) => {
-      const doc = collection.remove(this.toDbDocument());
-      if (!doc) {
-        return reject({ name: InternalError, errors: DbRemoveError });
-      }
-
-      return resolve({});
-    });
-  }
-
-  toDbDocument() {
-    const { linkURL } = this.properties;
-
-    return {
-      ...this.rawId && { $loki: this.rawId },
-      linkURL,
-    };
-  }
-
-  toJSON() {
-    return {
-      ...this.properties,
-      meta: { ...this.meta },
-    };
-  }
-}
-
-const LoadById = (collection, id) => {
-  const doc = collection.findOne({ $loki: id });
-  return doc === null ? null : new Link(doc);
-};
-
-const LoadByLinkURL = (collection, linkURL) => {
-  const doc = collection.findOne({ linkURL });
-  return doc === null ? null : new Link(doc);
-};
-
-const LoadLinks = collection =>
-  collection.find().map(doc => new Link(doc));
-
-
-module.exports = {
-  LinkSchema,
-  Link,
-  LoadByLinkURL,
-  LoadLinks,
-  LoadById,
-};
+module.exports = LinkPackage;
