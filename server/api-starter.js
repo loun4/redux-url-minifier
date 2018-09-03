9e56f2e0dccb995fa9e440a2a7d81936b62cc4a5
Iheb <iheb02@yahoo.fr>
2018-09-03 12:20:42 +0200

    [api] Expose available port on server start & use nodemode locally

--- server/api-starter.js
+++ server/api-starter.js
@@ -4,7 +4,7 @@ const Db = require('./lib/connectivity/db');
 const server = require('./lib/server');
 
 Db.init().then((db) => {
-  console.log('NOTICE', 'server ready');
+  console.log('NOTICE', 'Listening on port', process.env.PORT);
   return server(db);
 }).catch((e) => {
   console.log('ERROR', e);
