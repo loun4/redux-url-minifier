5bae45d2bba7bac4cae0eca4224ce4579b9c3c2a
Iheb <iheb02@yahoo.fr>
2018-09-04 18:49:30 +0200

    [app] Remove useless loading condition & better link preview management

--- client/src/containers/main.js
+++ client/src/containers/main.js
@@ -1,10 +1,9 @@
 
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
 import { saveEntityData } from '../actions/api';
-import Loader from '../components/loader';
 import LinkForm from '../components/link-form';
 import LinkItem from '../components/link-item';
 
@@ -14,36 +13,26 @@ class Main extends Component {
     linkForm: PropTypes.shape({
       linkURL: PropTypes.shape({}),
     }).isRequired,
-    link: PropTypes.shape({
-      isFetching: PropTypes.bool,
-      isSaving: PropTypes.bool,
-      models: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string,
-        linkURL: PropTypes.string,
-        shortURL: PropTypes.string,
-      })),
-    }).isRequired,
+    url: PropTypes.string,
     saveEntityData: PropTypes.func.isRequired,
     resetForm: PropTypes.func.isRequired,
   }
+
+  static defaultProps = {
+    url: null,
+  };
 
   handleLinkCreate = (model) => {
     this.props.saveEntityData({ entity: 'link', model });
   }
 
   render() {
-    const { linkForm, link, resetForm } = this.props;
-
-    if (link.isFetching) {
-      return <Loader />;
-    }
+    const { linkForm, resetForm, url } = this.props;
 
     return (
       <div>
         <LinkForm form={linkForm} onSubmit={this.handleLinkCreate} onReset={resetForm} />
-        {link.models.length > 0 && (
-          <LinkItem url={[...link.models].shift().shortURL} />
-        )}
+        {url && <LinkItem url={url} />}
       </div>
     );
   }
@@ -54,7 +43,7 @@ const mapStateToProps = ({
   entities: { link },
 }) => ({
   linkForm,
-  link,
+  url: link.models.length > 0 ? [...link.models].shift().shortURL : null,
 });
 
 // Allow dispatchProps to be overriden
